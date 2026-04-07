7ce26b3702c33a2429bc75b61d4ac687067be068
Samuel Almeida <almedasamuel0@gmail.com>
2026-04-06 23:30:56 -0300

    Fix output formatting for Fahrenheit prompt

--- ex15.c
+++ ex15.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     float Celsius, Fahrenheit;
-    printf("Coloque a temperatura em Fahrenheit: ");
+    printf("Coloque a temperatura em Fahrenheit:\n");
     scanf("%f", &Fahrenheit);
     Celsius = 5*(Fahrenheit - 32) / 9;
     printf("A temperatura em Celsius é: %.2f\n", Celsius);
